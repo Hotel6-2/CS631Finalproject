9b87e6bac7461542ebd5621ab4f515e82bfc4093
Hotel6-2 <bf234@njit.edu>
2024-12-04 18:06:30 -0500

    Update index.php
    added more pages

--- index.php
+++ index.php
@@ -8,7 +8,13 @@
 <body>
     <h1>Welcome to the Banking System</h1>
     <a href="view_employees.php">View Employees</a><br>
-    <a href="add_customer.php">Add New Customer</a><br>
+    <a href="add_employees.php">Manage Employees</a><br>
     <a href="view_customers.php">View Customers</a><br>
+    <a href="add_customer.php">Manage Customers</a><br>
+    <a href="view_Branches.php">View Branches</a><br>
+    <a href="add_branch.php">Manage Branches</a><br>
+    <a href="view_Accounts.php">View Accounts</a><br>
+    <a href="add_account.php">Manage Accounts</a><br>
+ 
 </body>
 </html>
